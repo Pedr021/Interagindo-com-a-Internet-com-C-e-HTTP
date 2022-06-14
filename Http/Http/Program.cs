bcfbab36abab0396ae6d543684df0b146c8f97dd
Pedro Canuto <104801029+Pedr021@users.noreply.github.com>
2022-06-14 08:29:58 -0300

    Add files via upload

---
+++ Http/Http/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HTTPp
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            ReqList();
+            ReqUnica();
+           Console.ReadLine();
+        }
+
+
+        static void ReqList()
+        {
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
+            requisicao.Method = "GET";
+            using (var resposta = requisicao.GetResponse())
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+                foreach (Tarefa tarefa in tarefas)
+                {
+                    tarefa.Exibir();
+                }
+
+
+                stream.Close();
+                resposta.Close();
+            }
+        }
+
+        static void ReqUnica ()
+        { 
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/5");
+            requisicao.Method = "GET";
+            using (var resposta = requisicao.GetResponse())
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                Tarefa tarefa = JsonConvert.DeserializeObject<Tarefa>(dados.ToString());
+
+                tarefa.Exibir();
+
+
+                stream.Close();
+                resposta.Close();
+            }
+        }
+    }
+}
